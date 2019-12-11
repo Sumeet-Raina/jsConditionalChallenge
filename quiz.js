ca07f4d12538d80107ea6bb80fa0002d63ebb5e0
sumeet978 <searchsumeet@gmail.com>
2019-12-11 13:38:39 +0000

    Added if conditions

--- quiz.js
+++ quiz.js
@@ -8,3 +8,20 @@ var question2 = prompt("Name India's national bird?");
 var question3 = prompt("What is India's national game?");
 var question4 = prompt("Name the prime-minister of India?");
 var question5 = prompt("Name the economic capital of India?");
+
+//added if condiions
+if (question1.toUpperCase() === 'DELHI') {
+  numberOfCorrectAnswers += 1;
+} 
+if (question2.toUpperCase() === 'PEACOCK') { 
+  numberOfCorrectAnswers += 1;
+}
+if (question3.toUpperCase() === 'HOCKEY') { 
+  numberOfCorrectAnswers += 1;
+}
+if (question4.toUpperCase() === 'MODI') { 
+  numberOfCorrectAnswers += 1;
+}
+if (question5.toUpperCase() === 'MUMBAI') { 
+  numberOfCorrectAnswers += 1;
+}
