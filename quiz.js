add9017246e640dc23422dea92b3b6e40552f5b4
sumeet978 <searchsumeet@gmail.com>
2019-12-11 13:37:22 +0000

    Added questions to js file

---
+++ quiz.js
@@ -0,0 +1,10 @@
+//quiz begins, no answers correct
+
+var numberOfCorrectAnswers = 0;
+
+//ask questions
+var question1 = prompt("What is the capital of India?");
+var question2 = prompt("Name India's national bird?");
+var question3 = prompt("What is India's national game?");
+var question4 = prompt("Name the prime-minister of India?");
+var question5 = prompt("Name the economic capital of India?");
